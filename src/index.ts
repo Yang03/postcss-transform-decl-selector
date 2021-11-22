9672296e2d3fe0092a104b0689911a3f0da0ebca
Yang <yangpan@zhongan.com>
2021-11-22 20:09:01 +0800

    0.0.1

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import type { Declaration, Root } from 'postcss';
+
+type Rule = {
+  selector: Array<string>;
+  transform: Function;
+}
+
+function transform(root: Root, rule: Rule) {
+  root.walkDecls((decl: Declaration) => {
+     // @ts-ignore
+    if (rule?.selector.includes(decl?.parent?.selector!)) {
+      rule.transform(decl);
+    }
+  })
+}
+
+const plugin = (rule: Rule) => {
+  return {
+    postcssPlugin: 'postcss-transform-selector',
+    Once(root: Root) {
+      transform(root, rule);
+    },
+  }
+}
+plugin.postcss = true;
+
+module.exports = plugin;
